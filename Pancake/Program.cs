56154c8973a0d4511e8adb8e8a75b7264469e96b
GD138 <10gday@SWA-233-20>
2023-04-18 13:10:02 +0100

    Add project files.

---
+++ Pancake/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("How many pancakes are you looking to make?");
+int pancake = Convert.ToInt32(Console.ReadLine());
+
+int number = 4 + (pancake + pancake % 2);
+Console.WriteLine(number);
+
